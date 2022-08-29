b4cd202075b81d1980505ae75d63c519a4097ae1
ArtRiaz <111689234+artriaz@users.noreply.github.com>
2022-08-29 13:37:21 +0000

    adding solution l_3HW01.01

---
+++ lessons_3_hw_1/01.py
@@ -0,0 +1,23 @@
+#Поменять местами  a и b
+a = 'Victoria'
+b = 'Regina'
+c = a,b = b,a
+print(c)
+
+
+
+a = list(range(1,11))
+b = list(range(5,16))
+a,b = b,a
+print('Значения списка а =',a)
+print('Значения списка b =',b)
+
+
+
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+a = a + b
+b = a - b
+a = a - b
+print('Значение а = ',a)
+print('Значение b = ',b)
