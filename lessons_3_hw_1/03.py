b27ae70e9d2a43fb938baded0828ea231bf5e500
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-01 13:22:47 +0200

    adding solution L03HW02.03

---
+++ lessons_3_hw_1/03.py
@@ -0,0 +1,8 @@
+# snake_case это формат написания слов без пробелов разделяющее нижнее подчеркивание funny__one__day
+# CamelCase - это формат написание слов с заглавной буквы и вместе FunnyOneDay
+print('Преобразование snake_case в Camel Case')
+a = input('Введите слова в формате snake_case: ')
+b = a.replace('_',' ')
+c = b.title()
+d = c.replace(' ','')
+print('Слова в формате Camel Case:',d)
