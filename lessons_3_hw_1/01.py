df09e61e1dbbf9e11e7fdf97e9405d1277708b52
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-02 13:43:13 +0200

    adding solution L03HW02.05 + change first exercise

--- lessons_3_hw_1/01.py
+++ lessons_3_hw_1/01.py
@@ -1,16 +1,22 @@
 #Поменять местами  a и b
 a = 'Victoria'
 b = 'Regina'
-c = a,b = b,a
-print(c)
+
+c = a
+a = b
+b = c
+
+print('Имя а =', a)
+print('Имя b =', b)
 
 
 
 a = list(range(1,11))
 b = list(range(5,16))
 a,b = b,a
-print('Значения списка а =',a)
-print('Значения списка b =',b)
+
+print('Значения списка а =', a)
+print('Значения списка b =', b)
 
 
 
@@ -19,5 +25,5 @@ b = int(input('Введите число b: '))
 a = a + b
 b = a - b
 a = a - b
-print('Значение а = ',a)
-print('Значение b = ',b)
+print('Значение а = ', a)
+print('Значение b = ', b)
