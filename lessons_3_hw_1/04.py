856835af276f1af14b5e614bb184bc4c03b964d4
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-02 17:46:29 +0200

    adding solution L03HW02.05 + corrected the mistake

--- lessons_3_hw_1/04.py
+++ lessons_3_hw_1/04.py
@@ -1,16 +1,16 @@
- Имя Фамилия кол-во лет
+# Имя Фамилия кол-во лет
 print('Привет , чтобы узнать возраст человека введите по такому примеру Taras Shevchenko*1814-03-09*1861-03-10')
 a =  input('Ввод: фамилия имя * дата рождения ГГГГ-ММ-ЧЧ * сегодняшняя дата ГГГГ-ММ-ЧЧ ')
-c = a.replace('*',' ')
-b = c.split()
 
-name = b[0]+ ' '+b[1]
-data1 = (b[3])
+b = a.split(sep='*')
+name = b[0]
 
+data1 = b[1]
+data2 = b[2]
 age1 = int(data1[:4])
-data2 = (b[2])
 age2 = int(data2[:4])
-age = age1 - age2
+age = age2 - age1
 
 print('Name: ', name)
 print('Age: ', age)
+
