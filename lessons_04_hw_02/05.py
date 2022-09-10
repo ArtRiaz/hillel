01b792183d76e3c5d8fafb92bd2b9f41e1ff6496
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-10 19:31:27 +0200

    adding solution L04HW03.05

---
+++ lessons_04_hw_02/05.py
@@ -0,0 +1,11 @@
+text = input('Введите текст: ')
+def stroka():
+   print(text[2])
+   print(text[-2])
+   print(text[:5])
+   print(text[:-2])
+   print(text[::2])
+   print(text[1::2])
+   print(text[::-2])
+   print(len(text))
+stroka()
