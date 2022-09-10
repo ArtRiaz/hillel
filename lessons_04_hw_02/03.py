a62ca5d191cb14018a48e62fc655eb38e34702f9
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-10 19:17:24 +0200

    adding solution L04HW03.03

---
+++ lessons_04_hw_02/03.py
@@ -0,0 +1,15 @@
+# Вычислить площадь и периметр прямоугольного треугольника
+
+import math
+
+print('Введите значение двух катетов треугольника')
+a = float(input('Введите длинну треугольника: '))
+b = float(input('Введите высоту треугольника: '))
+
+
+def triangle_area_and_perimeter(a, b):
+    c = math.sqrt(a ** 2 + b ** 2)  # Находим гипотинузу
+    return a * b / 2, a + b + c
+
+
+print(triangle_area_and_perimeter(a, b))
