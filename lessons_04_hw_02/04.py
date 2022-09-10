5b1f3575dcca68ba0c6a2ed68379257e64669809
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-10 19:26:54 +0200

    adding solution L04HW03.04

---
+++ lessons_04_hw_02/04.py
@@ -0,0 +1,5 @@
+a = int(input('Please enter an integer number: '))
+b = a + 1
+c = a - 1
+print('Next number for number', a, 'is', b, end='.\n')
+print('Previous number for number', a, 'is', c, end='.')
