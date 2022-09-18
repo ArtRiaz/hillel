3905b50e1f0485cde7d88325645616a3c43bd277
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-18 18:39:13 +0200

    L05_HW_04.06

---
+++ lessons_05_hw_03/06.py
@@ -0,0 +1,16 @@
+"""Position
+number Fibbonachi"""
+number = 20
+
+
+def fibbonachi(number):
+    if number <= 1:
+        return number
+    else:
+        return fibbonachi(number - 1) + fibbonachi(number - 2)
+
+
+for i in range(number):
+    print(fibbonachi(i))
+
+fibbonachi(number)
