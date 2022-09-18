f1cb4864ca0b662c020d33b23e60866206a24bb5
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-18 18:20:28 +0200

    L05_HW_04.03+adding solution

--- lessons_05_hw_03/03.py
+++ lessons_05_hw_03/03.py
@@ -1,4 +1,4 @@
-"""Calculate
+"""Calculate 
 intersect circles"""
 
 import math
@@ -18,13 +18,12 @@ def circles_intersect(x1, y1, r1, x2, y2, r2):
     diametr = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     return diametr
 
+
 if circles_intersect(x1, y1, r1, x2, y2, r2) < r1 - r2:
     print('Circle 2 is in Circle 1')
-elif  circles_intersect(x1, y1, r1, x2, y2, r2) < r2 -r1:
+elif circles_intersect(x1, y1, r1, x2, y2, r2) < r2 - r1:
     print('Circle 1 is in Circle 2')
-elif  circles_intersect(x1, y1, r1, x2, y2, r2) > r1 + r2:
+elif circles_intersect(x1, y1, r1, x2, y2, r2) > r1 + r2:
     print('Circumference of circle 1 and circle 2 intersect')
 else:
     print('Circle 1 and Circle 2 do not overlap')
-
-
