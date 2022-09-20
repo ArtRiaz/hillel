87fe287cd6123eb68a05db1b78733536b4f619ce
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-20 14:27:53 +0200

    L05_HW_04.04 + new solution

--- lessons_05_hw_03/04.py
+++ lessons_05_hw_03/04.py
@@ -3,7 +3,9 @@ leap year"""
 print('Do you want to know if the year is a leap year?')
 year = int(input('Enter year: '))
 
-if year % 4 == 0 and year % 400 == 0:
+if year % 100 == 0 and year % 400 != 0:
+    print('No')
+elif year % 4 == 0:
     print('Yes')
-elif year % 100 != 0:
+else:
     print('No')
