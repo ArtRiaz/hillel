87fd72bcb9244481315d75a035fd2cbf4e0cb1c6
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-20 15:07:08 +0200

    L05_HW_04.03 + new solution

--- lessons_05_hw_03/03.py
+++ lessons_05_hw_03/03.py
@@ -1,25 +1,25 @@
 """Calculate
 intersect circles"""
 
-#print('Enter the coordinates and radius of circle number one')
-#x1 = float(input('Enter the coordinates x: '))
-#y1 = float(input('Enter the coordinates y: '))
-#r1 = float(input('Enter radius: '))
+print('Enter the coordinates and radius of circle number one')
+x1 = float(input('Enter the coordinates x: '))
+y1 = float(input('Enter the coordinates y: '))
+r1 = float(input('Enter radius: '))
 
-#print('Enter the coordinates and radius of circle number two')
-#x2 = float(input('Enter the coordinates x: '))
-#y2 = float(input('Enter the coordinates y: '))
-#r2 = float(input('Enter radius: '))
-
-
-#def circles_intersect(x1, y1, r1, x2, y2, r2):
-#    distance = (x1 - x2) ** 2 + (y1 - y2) ** 2
-#    return (r2 - r1) **2 <= distance <= (r1 + r2) ** 2
+print('Enter the coordinates and radius of circle number two')
+x2 = float(input('Enter the coordinates x: '))
+y2 = float(input('Enter the coordinates y: '))
+r2 = float(input('Enter radius: '))
 
 
-#if circles_intersect(x1, y1, r1, x2, y2, r2) is True:
-#    print('Circle 1 and Circle 2 intersect')
-#else:
+def circles_intersect(x1, y1, r1, x2, y2, r2):
+    distance = (x1 - x2) ** 2 + (y1 - y2) ** 2
+    return (r2 - r1) **2 <= distance <= (r1 + r2) ** 2
+
+
+if circles_intersect(x1, y1, r1, x2, y2, r2) is True:
+    print('Circle 1 and Circle 2 intersect')
+else:
     print('Circle 1 and Circle 2 do not overlap')
 
 circles_intersect(x1, y1, r1, x2, y2, r2)
