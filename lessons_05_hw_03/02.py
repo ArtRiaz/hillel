6fbcc18601c1bf67650fa1e901984e7d4bc4a0b0
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-21 20:48:17 +0200

    L05_HW_04.02 + new solution

--- lessons_05_hw_03/02.py
+++ lessons_05_hw_03/02.py
@@ -1,3 +1,6 @@
+"""Solution of
+quadratic equations"""
+
 import math
 
 a = float(input('Enter meaning a = '))
@@ -6,11 +9,16 @@ c = float(input('Enter meaning c = '))
 
 
 def solve_quadratic_equation(a, b, c, ):
-    discriminant = b ** 2 - 4 * a * c  # Находим дискриминант
-    x1 = (-b + math.sqrt(discriminant)) / (2 * a)  # Значение если  discriminant > 0
-    x2 = (-b - math.sqrt(discriminant)) / (2 * a)  # Значение если  discriminant > 0
-    return x1, x2  # Возвращаем два значения
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        return x1, x2
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        return x, None
+    else:
+        return None, None
 
 
 print(solve_quadratic_equation(a, b, c, ))
-    pass
