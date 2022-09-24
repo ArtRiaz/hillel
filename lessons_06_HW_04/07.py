cf93e1bf78edf344e288d190c98e70a3a73d2596
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-24 16:47:37 +0200

    L05_HW_04.07

---
+++ lessons_06_HW_04/07.py
@@ -0,0 +1,19 @@
+"""Game guess
+ the number"""
+
+from random import randint
+
+number = randint(1,10)
+print('Enter the number 1-10')
+a = int(input('Enter number: '))
+
+while number != a:
+    if a > number:
+        print('Your number is greater than the given number')
+    elif a < number:
+        print('Your number is less than the given number')
+    print('Please try again')
+    a = int(input('Enter number: '))
+else:
+    print('You guessed!!!)))')
+
