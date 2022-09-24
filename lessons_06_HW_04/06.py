869ae9c13e9d37356d249eae38e854b07d41beb9
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-09-24 16:41:50 +0200

    L05_HW_04.06

---
+++ lessons_06_HW_04/06.py
@@ -0,0 +1,15 @@
+"""Calculates the sum
+ of the symbol code"""
+
+first = input('Enter meaning: ')
+last = input('Enter meaning: ')
+
+def sum_symbol_codes(first, last):
+    return summa
+summa = 0
+
+for i in range(ord(first),ord(last)):
+    summa = summa + i
+
+
+print(sum_symbol_codes(first, last))
