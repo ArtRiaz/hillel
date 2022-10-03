80bab82599b49e87159ae74cf58de343891f9f5f
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-10-03 13:42:07 +0200

    L06_HW_05.07+ change solution

--- lessons_07_hw_05/01.py
+++ lessons_07_hw_05/01.py
@@ -3,10 +3,11 @@ in list"""
 
 
 def indx(lst, elem):
-    if elem is None:
-        return None
-    my_list = lst[elem]
-    return my_list
+    for index, value in enumerate(lst, 0):
+        if elem == value:
+            return index
+        elif value is None:
+            return None
 
 
-indx([1, 2.0, ['b'], 'the final list'], 2)
+indx([1, 2.0, ['b'], 'the final list'], 1)
