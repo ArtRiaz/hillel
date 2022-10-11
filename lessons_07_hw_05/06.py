0b566bbdfcc6d0c152b30053772b15c74961718f
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-10-11 10:04:32 +0200

    L06_HW_06.06+ change solution

--- lessons_07_hw_05/06.py
+++ lessons_07_hw_05/06.py
@@ -3,30 +3,33 @@ import random
 
 def pemrtuate(texts):
     text_fynal = []
-
     texts = texts.split()
 
     for text in texts:
         text_list = []
-        if len(text) == 1:
+        if len(text) <= 2:
             text_fynal.append(text)
         else:
             for symbol in text:
                 text_list.append(symbol)
 
-            first_symbol = text_list[0]
-            last_symbol = text_list[- 1]
+        first_symbol = text_list[0]
+        last_symbol = text_list[- 1]
 
-            del text_list[0]
-            del text_list[- 1]
+        text_list.pop(0)
+        text_list.pop(-1)
 
-            random.shuffle(text_list)
-            text_list = ''.join(text_list)
-            my_text = first_symbol + text_list + last_symbol
-            text_fynal.append(my_text)
+        my_text = []
+        while len(text_list) != 0:
+            b = text_list[:3]
+            random.shuffle(b)
+            my_text = ''.join(b)
+            del text_list[:3]
+            pass
 
-    return ' '.join(text_fynal)
 
 
-texts = str(input('Введите предложение: \n >>> '))
-print(pemrtuate(texts))
+
+texts = str(input('Enter text\n>>>>> '))
+pemrtuate(texts)
+
