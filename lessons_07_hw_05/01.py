1661a31a09a82fc3961c1ca25bfa5a2c121d70e3
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-10-15 11:06:25 +0200

    L07_HW_05.01

--- lessons_07_hw_05/01.py
+++ lessons_07_hw_05/01.py
@@ -6,8 +6,7 @@ def indx(lst, elem):
     for index, value in enumerate(lst, 0):
         if elem == value:
             return index
-        elif elem is None:
-            return None
+
 
 
 indx([1, 2.0, ['b'], 'the final list'], 1)
