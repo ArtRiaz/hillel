6c7fc320db77f3e986bc0a1cc138271366844f1b
Artem <111689234+ArtRiaz@users.noreply.github.com>
2022-10-31 14:31:39 +0100

    lessons_10_HW_08.02

--- lessons_10_HW_08/02.py
+++ lessons_10_HW_08/02.py
@@ -2,19 +2,13 @@
 """Extract a list of
 names from a file"""
 
-def surname():
-
+def list_surname():
+    my_list = []
     with open('names.txt') as file:
-        my_list = []
-        a = list(file)
-        a.pop(0)
-        a.pop(-1)
-
-        for line in a:
-            b = line.split()
-            my_list.append(b[1])
+        for line in file:
+            surname = line.split()
+            my_list.append(surname[1])
         return my_list
 
 
-a = surname()
-print(a)
+list_surname()
